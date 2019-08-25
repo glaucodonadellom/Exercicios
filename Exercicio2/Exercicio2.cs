6346b0403916f12ba9ba520298db4822d444a462
Glauco Donadello <glaucodonadellom@gmail.com>
2019-08-25 19:33:06 -0300

    Initial commit

---
+++ Exercicio2/Exercicio2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Exercicio2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que inverte os numeros digitados.");
+            int valor = 0, unidade, dezena, centena,resultado;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Entre com um numero positivo de 3 digitos,");
+                    Console.WriteLine("Numero de 100 a 999.");
+                    valor = Convert.ToInt32(Console.ReadLine());
+
+                    if (valor < 100 || valor > 999)
+                    {
+                        Console.WriteLine("Valor digitado é invalido!");
+                    }
+                } while (valor < 100 || valor > 999);
+                unidade = valor / 100;
+                dezena = (valor % 100) / 10;
+                centena = ((valor % 100) % 10) / 1;
+                Console.WriteLine("valor digitado:  " + valor);
+                resultado = (centena * 100) + (dezena * 10) + (unidade);
+                Console.WriteLine("valor inverso :  " + resultado);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Valor digitado não é valido");
+                
+            }
+//            Console.WriteLine(unidade);
+//            Console.WriteLine(dezena);
+//            Console.WriteLine(centena);
+            finally {
+                Console.WriteLine("pressione qualquer tecla para sair");
+             Console.ReadKey();
+            }
+        }
+    }
+}
